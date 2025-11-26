acabb9638ef221cb23ff408cea50fdd1c09597d5
Габбасов Камиль Азатович <gabbasov@komus.lan>
2025-11-26 15:24:40 +0300

    debug incorrect deps in router_user

--- app/deps_and_routes/user_routes.py
+++ app/deps_and_routes/user_routes.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends
 from deps_and_routes.user_deps import basic_authoriz, check_valid_roles_from_db
+from schemas.model import User
+from functools import partial
 
 router_user = APIRouter(prefix='/user')
 
@@ -7,6 +9,6 @@ router_user = APIRouter(prefix='/user')
 async def protect_basic_auth():
     return {'message':'ok'}
 
-@router_user.get('/for_admin', dependencies=[Depends(check_valid_roles_from_db(('admin')))])
-async def protect_admin():
+@router_user.get('/for_admin')
+async def protect_admin(user:User = Depends(partial(check_valid_roles_from_db, need_roles={"admin"}))):
     return {'message':'ok ok'}
