87e082edf52a5684993187bea4b4664f188fe1f3
Габбасов Камиль Азатович <gabbasov@komus.lan>
2025-11-26 16:44:44 +0300

    try with reload true

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ api.include_router(router_player)
 api.include_router(router_user)
 
 if __name__=='__main__':
-    uvicorn.run("main:api", host="0.0.0.0", port=8000)
+    uvicorn.run("main:api", host="0.0.0.0", port=8000, reload=True)
